b03b338d4800465d47c509afaef52da5fcbc15b3
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-08-12 08:58:42 +0300

    copied old counter

---
+++ src/Components/counter/counter.jsx
@@ -0,0 +1,36 @@
+import { connect } from "react-redux";
+import {
+  increment,
+  decrement,
+  reset,
+} from "../../redux/counter/counter-actions";
+function Counter({ value, step, onIncrement, onDecrement, onReset }) {
+  return (
+    <div>
+      <p>{value}</p>
+      <button onClick={() => onIncrement(step)}>add {step}</button>
+      <button onClick={() => onDecrement(step)}>subtract {step}</button>
+      <button onClick={() => onReset()}>reset</button>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  // console.log(state);
+  return {
+    value: state.counter.value,
+    step: state.counter.step,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  // console.log(increment(10));
+  // дозволяє передати методи для
+  return {
+    onIncrement: (value) => dispatch(increment(value)),
+    onDecrement: (value) => dispatch(decrement(value)),
+    onReset: () => dispatch(reset()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
